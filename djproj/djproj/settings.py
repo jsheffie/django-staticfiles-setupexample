871e68ce3750014bcd690116e3d5ffc8c1c3d17f
Jeff Sheffield <jeff.sheffield@gmail.com>
2014-01-02 23:55:56 -0600

    modifications to serve static files

--- djproj/djproj/settings.py
+++ djproj/djproj/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    BASE_DIR + '../myapp/templates',
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -36,6 +42,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp'
 )
 
 MIDDLEWARE_CLASSES = (
--- djproj/djproj/urls.py
+++ djproj/djproj/urls.py
@@ -1,12 +1,20 @@
 from django.conf.urls import patterns, include, url
+#from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+#from django.conf.urls.static import static
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
 
+
 urlpatterns = patterns('',
     # Examples:
+    url(r'^$', 'myapp.views.index', name='index'),
     # url(r'^$', 'djproj.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+#urlpatterns += staticfiles_urlpatterns()
+#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
