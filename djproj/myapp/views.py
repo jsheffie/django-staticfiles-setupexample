4b081e86b689cc24a600b9044679cde03b82525f
Jeff Sheffield <jeff.sheffield@gmail.com>
2014-01-05 15:50:58 -0600

    return json ( test )

--- djproj/myapp/views.py
+++ djproj/myapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
-
+from django.http import HttpResponse
 # Create your views here.
 def index( request ):
+
+#	import pdb; pdb.set_trace();
+#	return HttpResponse("""{ "ResponseCode": "Success"}""", content_type="application/json; charset=UTF-8")
 	return render_to_response('index.html')
